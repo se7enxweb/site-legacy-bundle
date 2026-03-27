6cef564569d7628b8e28b36a9cd78d306f55bc41
7x <info@se7enx.com>
2026-03-26 17:19:06 -0700

    Fix ImageVariationPass: use named service IDs not FQCN for eZ Platform 3.3

--- bundle/DependencyInjection/Compiler/ImageVariationPass.php
+++ bundle/DependencyInjection/Compiler/ImageVariationPass.php
@@ -4,27 +4,25 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\SiteLegacyBundle\DependencyInjection\Compiler;
 
-use eZ\Bundle\EzPublishCoreBundle\Imagine\VariationPathGenerator\OriginalDirectoryVariationPathGenerator;
-use eZ\Bundle\EzPublishCoreBundle\Imagine\VariationPurger\ImageFileVariationPurger;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ImageVariationPass implements CompilerPassInterface
 {
     /**
-     * Overrides built in Ibexa image variation purgers and path generators
+     * Overrides built in eZ Platform image variation purgers and path generators
      * to use ones specific for legacy.
      */
     public function process(ContainerBuilder $container): void
     {
         $container->setAlias(
             'ezpublish.image_alias.variation_purger',
-            ImageFileVariationPurger::class,
+            'ezpublish.image_alias.variation_purger.legacy_storage_image_file',
         );
 
         $container->setAlias(
             'ezpublish.image_alias.variation_path_generator',
-            OriginalDirectoryVariationPathGenerator::class,
+            'ezpublish.image_alias.variation_path_generator.original_directory',
         );
     }
 }
